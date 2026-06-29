f5f3f6771107b126483b50588ce5c93066829cc4
M <maznbjad@gmail.com>
2026-06-29 21:35:55 +0300

    update

--- run_barq.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title BARQ NEWS
-cd /d "%~dp0"
-python -m pip install -r requirements.txt
-start "BARQ MONITOR" /MIN python monitor_worker.py
-python -m streamlit run app.py
-pause
--- stop_barq.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python -c "from core import update_control; update_control(enabled=False)"
-echo Monitoring disabled.
-pause
